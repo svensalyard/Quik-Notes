5b6ec0c12bfc561a2d04602732219febfc1ba7b7
svensalyard <svensalyard@gmail.com>
2024-03-10 23:27:10 -0700

    Added README and Formmatted code

---
+++ README.md
@@ -0,0 +1,33 @@
+# Quik Notes
+
+### Description
+
+A quik application to take notes and save them to a webpage.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Useage](#useage)
+- [License](#license)
+- [Contact](#Contact)
+
+### Installation
+
+Download the zip or clone into the repository.
+
+### Useage
+
+Integrate it into a project or use a program like insomnia to use the routes.
+
+### License
+
+Protected under the: MIT ![License](https://img.shields.io/badge/License-MIT-yellow.svg) [MIT](https://opensource.org/licenses/MIT)
+
+### Contact
+
+Author: Sven Salyard
+
+Email: svensalyard@gmail.com
+
+Github: [My Github](https://github.com/svensalyard)
--- routes/notes.js
+++ routes/notes.js
@@ -1,53 +1,49 @@
-const apiRouter = require('express').Router();
-const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
-const util = require('util');
+const apiRouter = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
+const util = require("util");
 
 const readFromFile = util.promisify(fs.readFile);
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
+const writeToFile = (destination, content) => fs.writeFile(destination, JSON.stringify(content, null, 4), (err) => (err ? console.error(err) : console.info(`\nData written to ${destination}`)));
 
-  const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-  };
+const readAndAppend = (content, file) => {
+	fs.readFile(file, "utf8", (err, data) => {
+		if (err) {
+			console.error(err);
+		} else {
+			const parsedData = JSON.parse(data);
+			parsedData.push(content);
+			writeToFile(file, parsedData);
+		}
+	});
+};
 
-  apiRouter.get('/notes/:id', (req, res) => {
-    const notesId = req.params.id;
-    readFromFile('./db/db.json')
-      .then((data) => JSON.parse(data))
-      .then((json) => {
-        const result = json.filter((notes) => notes.id === notesId);
-        return result.length > 0
-          ? res.json(result)
-          : res.json('No note with that ID');
-      });
-  });
-  
-  apiRouter.post('/notes', (req, res) => {
-    const { title, text } = req.body;
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-      res.json(newNote);
-      console.log(res.json);
-    } else {
-      res.error('Error in adding tip');
-    }});
+apiRouter.get("/notes/:id", (req, res) => {
+	const notesId = req.params.id;
+	readFromFile("./db/db.json")
+		.then((data) => JSON.parse(data))
+		.then((json) => {
+			const result = json.filter((notes) => notes.id === notesId);
+			return result.length > 0 ? res.json(result) : res.json("No note with that ID");
+		});
+});
+
+apiRouter.post("/notes", (req, res) => {
+	const { title, text } = req.body;
+	if (req.body) {
+		const newNote = {
+			title,
+			text,
+			id: uuidv4(),
+		};
+
+		readAndAppend(newNote, "./db/db.json");
+		res.json(newNote);
+		console.log(res.json);
+	} else {
+		res.error("Error in adding tip");
+	}
+});
 
 module.exports = apiRouter;

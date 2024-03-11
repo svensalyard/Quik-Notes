13b44a5fdb4f8c6f59394f40ac0aa93182795840
svensalyard <svensalyard@gmail.com>
2024-03-10 22:35:14 -0700

    Added server file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.listen(PORT, () =>
+	console.log(`App listening at http://localhost:${PORT}`)
+);
